f06dc6fc151f08df1c7c82fb71240796d720e759
caesiumy <mn0658@naver.com>
2020-05-05 19:37:39 +0900

    feat: Update BookComponent to show small details

--- src/Components/BookComponent.js
+++ src/Components/BookComponent.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export default class BookComponent extends Component {
-  //   static propTypes = {
-  //     prop: PropTypes,
-  //   };
+  static propTypes = {
+    book: PropTypes.object.isRequired,
+  };
 
   render() {
+    const { book } = this.props;
     return (
       <li>
         <div className="book">
@@ -16,8 +17,7 @@ export default class BookComponent extends Component {
               style={{
                 width: 128,
                 height: 193,
-                backgroundImage:
-                  'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+                backgroundImage: `url(${book.imageLinks.smallThumbnail})`,
               }}
             />
             <div className="book-shelf-changer">
@@ -32,8 +32,15 @@ export default class BookComponent extends Component {
               </select>
             </div>
           </div>
-          <div className="book-title">To Kill a Mockingbird</div>
-          <div className="book-authors">Harper Lee</div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">
+            {book.authors.map((author) => (
+              <span key={author}>
+                - {author}
+                <br />
+              </span>
+            ))}
+          </div>
         </div>
       </li>
     );
